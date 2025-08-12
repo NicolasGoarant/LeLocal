5b9d52b4d7d3a8646e8828157b14788d31ba250f
NicolasGoarant <nicolas.goarant@hotmail.fr>
2025-08-12 10:43:46 +0200

    Mailer: expose ApplicationHelper (category_display) to email templates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,11 @@ module ApplicationHelper
 def category_display(cat)
   return "" if cat.blank?
   cat.respond_to?(:name) ? cat.name.to_s : cat.to_s
+  # Affiche le libellé humain d\047une catégorie Space
+  def category_display(val)
+    pair = Space::CATEGORIES.find { |label, key| key.to_s == val.to_s }
+    pair ? pair.first : (val.to_s.strip.empty? ? "Autre" : val.to_s.humanize)
+  end
 end
 
 end
